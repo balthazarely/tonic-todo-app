ee5d0dee56714a894efada1d69a1690ecf408a9a
Balthazar <balthazar.elyj@gmail.com>
2021-09-19 12:26:27 -0600

    setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/global.css';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { Todo } from './pages/Todo';
+import { SingleTodo } from './pages/SingleTodo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/repo/:author/:name" component={Todo} />
+          <Route exact path="/" component={SingleTodo} />
+        </Switch>
+      </Router>
     </div>
   );
 }
